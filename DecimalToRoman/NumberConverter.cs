3ed419c8ea19022c246adde9f8eaaee05d228e21
Miguel Rivera <mrivera@universal.com.do>
2018-10-03 09:59:49 -0400

    Exercise finished

--- DecimalToRoman/NumberConverter.cs
+++ DecimalToRoman/NumberConverter.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 
+// This is a namespace for the class
 namespace DecimalToRoman
 {
+    // This class is an utility for convert numbers from a format to other
     public static class NumberConverter
     {
-        private const int MaximumDecimalNumber = 3000;
-        private const int MinimumDecimalNumber = 0;
+        // Maximum decimal number for convert to roman numbers
+        private const int MaximumDecimalRomanNumber = 3000;
+        private const int MinimumDecimalRomanNumber = 0;
 
         private static readonly string[] RomanThousands = { "", "M", "MM", "MMM" };
         private static readonly string[] RomanHundreds = { "", "C", "CC", "CCC", "CD",
                                                             "D", "DC", "DCC", "DCCC", "CM" };
 
-        private static readonly string[] RomanTen = { "", "X", "XX", "XXX", "XL", "L", "LX",
+        private static readonly string[] RomanTens = { "", "X", "XX", "XXX", "XL", "L", "LX",
                                                             "LXX", "LXXX", "XC" };
 
         private static readonly string[] RomanOnes = { "", "I", "II", "III", "IV", "V", "VI",
@@ -23,51 +26,65 @@ namespace DecimalToRoman
             ValidateNumberIsPositive(number);
             ValidateNumberIsLessOrEqualThanMaximum(number);
 
-            return "";
+            return IntToRoman(Convert.ToInt32(number));
         }
 
         private static void ValidateNumberIsInteger(double number)
         {
-            if ((number % 1) == 0)
+            if ((number % 1) != 0)
             {
                 throw new Exception("El numero debe ser un entero");
             }
         }
 
-        private static void ValidateNumberIsLessOrEqualThanMaximum(double number)
-        {
-            if (number < MaximumDecimalNumber)
-            {
-                throw new Exception($"El numero debe ser menor que {MaximumDecimalNumber}");
-            }
-        }
-
         private static void ValidateNumberIsPositive(double number)
         {
-            if (number > MinimumDecimalNumber)
+            if (number < MinimumDecimalRomanNumber)
             {
                 throw new Exception("El numero debe ser un numero positivo");
             }
         }
 
-        private string IntToRoman(int number)
+        private static void ValidateNumberIsLessOrEqualThanMaximum(double number)
+        {
+            if (number > MaximumDecimalRomanNumber)
+            {
+                throw new Exception($"El numero debe ser menor que {MaximumDecimalRomanNumber}");
+            }
+        }
+
+        private static string IntToRoman(int number)
         {
             string romanThousands = GetRomanThousands(number);
             string romanHundreds = GetRomanHundreds(number);
-            roman += ten[(int)(num / 10) % 10];
-            roman += ones[num % 10];
-
-            return romanThousands + romanHundreds;
+            string romanTens = GetRomanTens(number);
+            string romanOnes = GetRomanOnes(number);
+            return romanThousands + romanHundreds + romanTens + romanOnes;
         }
 
         private static string GetRomanThousands(int number)
         {
-            return RomanThousands[(int)(number / 1000) % 10];
+            return RomanThousands[GetIndexForNumber(number, 1000)];
         }
 
         private static string GetRomanHundreds(int number)
         {
-            return RomanHundreds[(int)(number / 100) % 10];
+            return RomanHundreds[GetIndexForNumber(number, 100)];
+        }
+
+        private static string GetRomanTens(int number)
+        {
+            return RomanTens[GetIndexForNumber(number, 10)];
+        }
+
+        private static string GetRomanOnes(int number)
+        {
+            return RomanOnes[GetIndexForNumber(number, 1)];
+        }
+
+        private static int GetIndexForNumber(int number, int factor)
+        {
+            return (number / factor) % 10;
         }
     }
 }
--- DecimalToRoman/Program.cs
+++ DecimalToRoman/Program.cs
@@ -6,9 +6,27 @@ namespace DecimalToRoman
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ingrese un numero decimal para convertirlo a notacion romana");
-            string number = Console.ReadLine();
+            string inputNumber = RequestDecimalNumber();
+            string romanNumber = GetRomanNumberFromDecimal(inputNumber);
+            WriteAndWait(romanNumber);
+        }
 
+        private static string RequestDecimalNumber()
+        {
+            Console.WriteLine("Ingrese un numero decimal para convertirlo a notacion romana");
+            return Console.ReadLine();
+        }
+
+        private static string GetRomanNumberFromDecimal(string inputNumber)
+        {
+            double decimalNumber = Convert.ToDouble(inputNumber);
+            return NumberConverter.GetRomanNumberFromDecimal(decimalNumber);
+        }
+
+        private static void WriteAndWait(string message)
+        {
+            Console.WriteLine(message);
+            Console.ReadLine();
         }
     }
 }
